402029c77ef8f0619f726e64e15600ddf6afcf88
Alexey R <alex666alex@mail.ru>
2021-03-01 16:38:00 +0300

    styled-comp for charDetails/header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import { HeaderBlock, HeaderTitle, HeaderLinks } from './styles';
 
 export default class Header extends Component {
 	state = {}
 	render() {
 		return (
-			<div>header</div>
+			<HeaderBlock>
+				<HeaderTitle>
+					<a href="#">
+						Game of Thrones DB
+                </a>
+				</HeaderTitle>
+				<HeaderLinks>
+					<li>
+						<a href="#">Characters</a>
+					</li>
+					<li>
+						<a href="#">Houses</a>
+					</li>
+					<li>
+						<a href="#">Books</a>
+					</li>
+				</HeaderLinks>
+			</HeaderBlock>
 		);
 	}
 }
