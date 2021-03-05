0548bdca0b2f78cee9587d3abb25e3107cf3d699
Alexey R <alex666alex@mail.ru>
2021-03-05 15:40:46 +0300

    fix styles for app

---
+++ src/components/app/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ToggleBtn = styled.button`
+	margin-bottom: 20px;
+	width: 100%;
+	height:50px;
+	background-color: rgb(19,35,44);
+	font-size: 20px
+`;
+
+export default ToggleBtn;
