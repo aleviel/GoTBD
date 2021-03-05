54278d99f90fbc2ba9ea19a7d3675aba099ea4de
Alexey R <alex666alex@mail.ru>
2021-03-05 15:42:09 +0300

    toggle button to show randomChar

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,54 @@
-import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import React, {Component} from 'react';
+import {Container, Row, Col} from 'reactstrap';
 import CharDetails from '../charDetails/charDetails';
 import Header from '../header/header';
 import ItemList from '../itemList/itemList';
 import RandomChar from '../randomChar/';
+import ToggleBtn from './styles';
 
 export default class App extends Component {
-	state = {}
-	render() {
-		return (
-			<>
-				<Container>
-					<Header />
-				</Container>
-				<Container>
-					<Row>
-						<Col lg={{ size: 5, offset: 0 }}>
-							<RandomChar />
-						</Col>
-					</Row>
-					<Row>
-						<Col md={{ size: 6, offset: 0 }}>
-							<ItemList />
-						</Col>
-						<Col md={{ size: 6, offset: 0 }}>
-							<CharDetails />
-						</Col>
-					</Row>
-				</Container>
-			</>
-		);
-	}
+    state = {
+        showChar: true
+    }
+
+    toggleChar = () => {
+        this.setState(state => {
+            return {
+                showChar: !state.showChar
+            }
+        })
+    }
+
+    render() {
+        const {showChar} = this.state;
+        const content = showChar ? <RandomChar/> : null;
+        return (
+            <>
+                <Container>
+                    <Header/>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                            <ToggleBtn
+                                type="button"
+                                className="btn btn-dark"
+                                onClick={this.toggleChar}>
+                                ToggleChar
+                            </ToggleBtn>
+                            {content}
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={{size: 6, offset: 0}}>
+                            <ItemList/>
+                        </Col>
+                        <Col md={{size: 6, offset: 0}}>
+                            <CharDetails/>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
 }
